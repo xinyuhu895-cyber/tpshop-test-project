e6a9c2df34995727dc0f5daa5b6acfc47ddb1cf2
xinyuhu895-cyber <xinyuhu895@gmail.com>
2026-03-19 14:24:19 +0800

    Remove unnecessary files from repo

--- addons/assistact/uninstall.sql
+++
@@ -1,5 +0,0 @@
-DROP TABLE IF EXISTS `__PREFIX__assist_award`;
-DROP TABLE IF EXISTS `__PREFIX__assist_leader`;
-DROP TABLE IF EXISTS `__PREFIX__assist_reward`;
-DROP TABLE IF EXISTS `__PREFIX__assist_supply`;
-DROP TABLE IF EXISTS `__PREFIX__promotion_assist`;
